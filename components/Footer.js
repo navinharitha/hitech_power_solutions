1f437c7ed5edc56fc56c5c4208e9269ade318590
navinharitha <navin@pmgs.lk>
2022-02-09 08:04:15 +0530

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+function Footer() {
+  return <h3>Footer</h3>;
+}
+
+export default Footer;
