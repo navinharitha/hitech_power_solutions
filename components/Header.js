a93f2333ce17f4348492e841789bbe960e622c81
navinharitha <navin@pmgs.lk>
2022-02-09 07:49:51 +0530

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+function Header() {
+  return <h3>Header</h3>;
+}
+
+export default Header;
